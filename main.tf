3a92b3391d4caaeefc9f8f99a8a54992aafaf684
Victor Galvez <vhgalvez@gmail.com>
2024-04-30 17:37:41 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,15 +36,16 @@ resource "libvirt_pool" "volumetmp" {
 
 locals {
   vm_instances = merge([
-    for vm_type, config in var.vm_count : {
-      for i in range(config.count) : "${vm_type}-${i + 1}" => {
-        cpus   = config.cpus,
-        memory = config.memory
+    for vm_type, config in var.vm_count : [
+      for i in range(config.count) : {
+        "${vm_type}-${i + 1}" = {
+          cpus   = config.cpus
+          memory = config.memory
+        }
       }
-    }
+    ]
   ]...)
 }
-
 resource "libvirt_volume" "base" {
   for_each = locals.vm_instances
   name     = "${each.key}-base"
