2ef5e971473cd060d2ea9f0d1ade751bf9881650
Victor Galvez <vhgalvez@gmail.com>
2024-04-30 16:45:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -119,32 +119,3 @@ data "ct_config" "vm-ignitions" {
 output "ip_addresses" {
   value = { for k, vm in libvirt_domain.vm : k => vm.network_interface[0].addresses[0] }
 }
-
-# variables.tf
-variable "base_image" {
-  description = "Path to the base VM image"
-  type        = string
-}
-
-variable "vm_count" {
-  description = "Map of VM types and their quantities, cpus, and memory"
-  type = map(object({
-    count : number
-    cpus : number
-    memory : number
-  }))
-}
-variable "cluster_name" {
-  description = "Name of the cluster"
-  type        = string
-}
-
-variable "cluster_domain" {
-  description = "Domain of the cluster"
-  type        = string
-}
-
-variable "ssh_keys" {
-  description = "SSH keys to inject into VMs"
-  type        = list(string)
-}
