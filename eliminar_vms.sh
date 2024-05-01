7e5b163d88d0a3bcff67d9721cc1d49120e030e1
Victor Galvez <vhgalvez@gmail.com>
2024-05-01 20:17:07 +0200

    Update eliminar_vms.sh

--- eliminar_vms.sh
+++ eliminar_vms.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define un arreglo con los nombres de las máquinas virtuales
-vms=("bastion1" "bootstrap1" "elasticsearch1" "freeipa1" "kibana1" "load_balancer1" "master-1" "master2" "master3" "nfs-1" "postgresql1" "worker1" "worker2" "worker3")
+vms=("bastion1" "bootstrap1" "elasticsearch1" "freeipa1" "kibana1" "load_balancer1" "master1" "master2" "master3" "nfs1" "postgresql1" "worker1" "worker2" "worker3")
 
 # Itera sobre el arreglo y elimina las máquinas virtuales
 for vm in "${vms[@]}"
