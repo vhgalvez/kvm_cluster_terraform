bacf82d18b0ee83061c744aa60fca7d0b2cb8aaa
Victor Galvez <vhgalvez@gmail.com>
2024-05-01 20:43:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
+# main.tf
 terraform {
   required_version = ">= 0.13"
+
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
@@ -35,36 +37,34 @@ resource "libvirt_pool" "volumetmp" {
 }
 
 resource "libvirt_volume" "base" {
-  for_each = var.vm_definitions
-
-  name   = "${each.key}-base"
+  name   = "${var.cluster_name}-base"
   source = var.base_image
   pool   = libvirt_pool.volumetmp.name
   format = "qcow2"
 }
 
 data "template_file" "vm-configs" {
-  for_each = var.vm_definitions
+  for_each = toset(var.vm_definitions)
 
   template = file("${path.module}/configs/machine-${each.key}-config.yaml.tmpl")
 
   vars = {
-    ssh_keys     = jsonencode(var.ssh_keys),
-    name         = each.key,
-    host_name    = "${each.key}.${var.cluster_name}.${var.cluster_domain}",
-    strict       = true,
+    ssh_keys     = jsonencode(var.ssh_keys)
+    name         = each.key
+    host_name    = "${each.key}.${var.cluster_name}.${var.cluster_domain}"
+    strict       = true
     pretty_print = true
   }
 }
 
 data "ct_config" "vm-ignitions" {
-  for_each = var.vm_definitions
+  for_each = toset(var.vm_definitions)
 
   content = data.template_file.vm-configs[each.key].rendered
 }
 
 resource "libvirt_ignition" "ignition" {
-  for_each = var.vm_definitions
+  for_each = toset(var.vm_definitions)
 
   name    = "${each.key}-ignition"
   pool    = libvirt_pool.volumetmp.name
@@ -72,7 +72,7 @@ resource "libvirt_ignition" "ignition" {
 }
 
 resource "libvirt_volume" "vm_disk" {
-  for_each = var.vm_definitions
+  for_each = toset(var.vm_definitions)
 
   name           = "${each.key}-${var.cluster_name}.qcow2"
   base_volume_id = libvirt_volume.base[each.key].id
@@ -81,11 +81,11 @@ resource "libvirt_volume" "vm_disk" {
 }
 
 resource "libvirt_domain" "machine" {
-  for_each = var.vm_definitions
+  for_each = toset(var.vm_definitions)
 
   name   = each.key
-  vcpu   = each.value.cpus
-  memory = each.value.memory
+  vcpu   = var.virtual_cpus
+  memory = var.virtual_memory
 
   network_interface {
     network_id     = libvirt_network.kube_network.id
@@ -105,7 +105,5 @@ resource "libvirt_domain" "machine" {
 }
 
 output "ip-addresses" {
-  value = {
-    for key, machine in libvirt_domain.machine : key => machine.network_interface.0.addresses[0] if length(machine.network_interface.0.addresses) > 0
-  }
+  value = { for key, machine in libvirt_domain.machine : key => machine.network_interface.0.addresses[0] if length(machine.network_interface.0.addresses) > 0 }
 }
