19df23d5a6d27c25c8db86bc96394c2f5b217d62
Victor Galvez <vhgalvez@gmail.com>
2024-05-03 21:59:22 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,8 +21,6 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-provider "ct" {}
-
 resource "libvirt_network" "kube_network" {
   name      = "kube_network"
   mode      = "nat"
@@ -44,9 +42,7 @@ resource "libvirt_volume" "base" {
 
 data "template_file" "vm-configs" {
   for_each = var.vm_definitions
-
   template = file("${path.module}/configs/machine-${each.key}-config.yaml.tmpl")
-
   vars = {
     ssh_keys     = jsonencode(var.ssh_keys),
     name         = each.key,
@@ -58,38 +54,34 @@ data "template_file" "vm-configs" {
 
 data "ct_config" "vm-ignitions" {
   for_each = var.vm_definitions
-
-  content = data.template_file.vm-configs[each.key].rendered
+  content  = data.template_file.vm-configs[each.key].rendered
 }
 
 resource "libvirt_ignition" "ignition" {
   for_each = var.vm_definitions
-
-  name    = "${each.key}-ignition"
-  pool    = libvirt_pool.volumetmp.name
-  content = data.ct_config.vm-ignitions[each.key].rendered
+  name     = "${each.key}-ignition"
+  pool     = libvirt_pool.volumetmp.name
+  content  = data.ct_config.vm-ignitions[each.key].rendered
 }
 
 resource "libvirt_volume" "vm_disk" {
-  for_each = var.vm_definitions
-
-  name           = "${each.key}-${var.cluster_name}.qcow2"
+  for_each      = var.vm_definitions
+  name          = "${each.key}-${var.cluster_name}.qcow2"
   base_volume_id = libvirt_volume.base.id
-  pool           = libvirt_pool.volumetmp.name
-  format         = "qcow2"
+  pool          = libvirt_pool.volumetmp.name
+  format        = "qcow2"
 }
 
 resource "libvirt_domain" "machine" {
   for_each = var.vm_definitions
-
-  name   = each.key
-  vcpu   = each.value.cpus
-  memory = each.value.memory
+  name     = each.key
+  vcpu     = each.value.cpus
+  memory   = each.value.memory
 
   network_interface {
     network_id     = libvirt_network.kube_network.id
     wait_for_lease = true
-    addresses      = [each.value.ip]  # Use the IP address specified in vm_definitions
+    addresses      = [each.value.ip]  # Assign static IP directly
   }
 
   disk {
